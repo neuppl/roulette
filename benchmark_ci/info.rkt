cc5c32413fec9a6d67f8afe0dc7d0cdbbd1173c1
Smaran-Teja-P <smaranteja32@gmail.com>
2025-09-15 20:21:53 -0400

    move benchmark_ci hashlang to root directory

---
+++ benchmark_ci/info.rkt
@@ -0,0 +1,7 @@
+#lang setup/infotab
+
+(define name "benchmark_ci")
+(define collection "benchmark_ci")
+(define deps '("racket" "json" "relation-lib" "make-log-interceptor")) ; or add any dependencies
+(define langs '(("benchmark_ci" "main.rkt")))
+
