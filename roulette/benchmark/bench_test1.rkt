cf01e15f51e210c02eefe7df883d187fb41a2ba2
Smaran-Teja-P <smaranteja32@gmail.com>
2025-09-16 23:16:08 -0400

    remove unneccessary benchmarks

--- roulette/benchmark/bench_test1.rkt
+++
@@ -1,8 +0,0 @@
-#lang benchmark_ci
-
-(define (slower-fn)
-  (sleep 1)
-  "Completed")
-
-  
-(slower-fn)
--- roulette/benchmark/bench_test2.rkt
+++
@@ -1,8 +0,0 @@
-#lang benchmark_ci
-
-(define (slower-fn)
-  (sleep 2)
-  "Completed")
-
-  
-(slower-fn)
