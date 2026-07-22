e4c5b0de64db66c1cb24d8441f6cc56530c38cbd
Smaran Teja <smaranteja32@gmail.com>
2026-07-22 13:42:07 -0400

    try self-hosted

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-raco pkg remove roulette-lib roulette; raco pkg install roulette-lib/ roulette/
+#!/bin/sh
+set -e
+
+PKGS="roulette-lib/ roulette/ gtp-util gtp-plot markdown"
+
+for p in $PKGS; do
+  raco pkg remove --auto --force "${p%/}" || true
+done
+
+raco pkg install --auto $PKGS
