7ddd67c7055b1a85e475ca38ec72a9afe2865157
Smaran Teja <smaranteja32@gmail.com>
2026-07-21 20:12:32 -0400

    fix munin.bif.rkt

--- roulette/benchmark/bayesian-networks/munin.bif.rkt
+++ roulette/benchmark/bayesian-networks/munin.bif.rkt
@@ -5,7 +5,8 @@
 
 
 (define (main)
-    (main* "munin.bif"
+  (benchmark
+    (main* "bayesian-networks/munin.bif"
       'L_SUR_CV_CA
       '(R_LNLW_MED_SEV
         R_LNLW_MED_PATHO
@@ -1047,4 +1048,4 @@
         L_SUR_DIFSLOW_CA
         L_SUR_DSLOW_CA
         L_SUR_ALLCV_CA
-        L_SUR_CV_CA)))
+        L_SUR_CV_CA))))
