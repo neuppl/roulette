15998fe5a163563693072c33584810e99945425d
Smaran Teja <smaranteja32@gmail.com>
2026-07-29 16:15:36 -0400

    try to fix workflow

--- update.sh
+++ update.sh
@@ -3,6 +3,4 @@ set -e
 
 PKGS="roulette-lib/ roulette/ gtp-util gtp-plot markdown"
 
-for p in $PKGS; do
-  raco pkg update --auto "${p%/}" || raco pkg install --auto "${p%/}"
-done
+raco pkg update --auto $PKGS || raco pkg install --auto $PKGS
